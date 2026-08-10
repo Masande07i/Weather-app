cf0a9085e022f460ca106cbdc86da9fa519bfea0
Masande07i <magoso.nondumo@gmail.com>
2026-08-10 22:20:41 +0200

    saved locations

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,20 @@ import {Settings} from './pages/Settings/Settings'
 import { SavedLocations } from './pages/SavedLocations/SavedLocations';
 import { Sidebar } from './components/Sidebar/Sidebar';
 import { Alerts } from './pages/Alerts/Alerts';
+import { useState } from 'react';
 
 
 
 function App() {
+   const [unit, setUnit] = useState<'C' | 'F'>('C');
  
 
     return (
     <div>
-      <Sidebar />
+      <Sidebar unit={unit} setUnit={setUnit}/>
       <Routes>
       <Route path ='/' element={<Home/>}/>
-      <Route path='/settings' element= {<Settings/>}/>
+      <Route path='/settings' element= {<Settings />}/>
       <Route path= '/saved-locations' element = {<SavedLocations/>}/>
       <Route path='alerts' element ={<Alerts/>}/>
       </Routes>
